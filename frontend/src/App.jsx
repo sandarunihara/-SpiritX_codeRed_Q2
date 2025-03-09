3ebd304fe04f203acfca4cdb76f520a72634d71a
ASHAN2021 <akh8130@gmail.com>
2025-03-09 13:55:17 +0530

    sign up and sign in front end connected to the back end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
+import SignUp from './pages/Signup';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   )
