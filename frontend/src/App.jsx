c0b7191a0899f6990004a6d3fe1142134c9a7b4d
NIHARA W.A.S <sandarunihara56@gmail.com>
2025-03-09 13:03:01 +0530

    Add admin panel components and update Home page layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home'
+import Adminpanel from './pages/Adminpanel';
+import AdminAddplayer from './pages/AdminAddplayer';
+import AdminPlayers from './pages/AdminPlayers';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Adminpanel/>} />
+        <Route path="/admin/addplayer" element={<AdminAddplayer/>} />
+        <Route path="/admin/player" element={<AdminPlayers/>} />
       </Routes>
     </Router>
   )
